0f5ab26caeee9f8cef2d8311ca081d8f7380c5b1
bruno <bmendez@froneus.com>
2021-03-12 16:59:06 -0300

    complete

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 const challengeFunction = function(cell1, cell2){
-    /* YOUR CODE HERE */
-    let result
-
-    return result
+    celdasPares = ["B","D","F","H"];
+    colorcell1 =  celdasPares.includes(cell1.charAt(0)) ^ Number(cell1.charAt(1))%2 === 0;
+    colorCell2 =  celdasPares.includes(cell2.charAt(0)) ^ Number(cell2.charAt(1))%2 === 0;
+    return colorcell1 ^ colorCell2 ? false : true;
 }
 
 module.exports = challengeFunction
